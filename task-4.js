93155050f8790d5d4ef3c89ebaa7122ec9e41b10
mkrafiul13453 <mkrafiul13453@gamil.com>
2026-01-29 19:51:50 +0600

    task-4 is done

---
+++ task-4.js
@@ -0,0 +1,10 @@
+const fruits=["apple","orange","jackfruit","mango"];
+const line="The name of country is Bangladesh."
+
+if(Array.isArray(line)){
+    console.log("yes this is  an array.");
+}
+
+else{
+    console.log("This is not an array.")
+}
