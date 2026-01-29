bac3b5ae38d3583e9e7488908f23127460269782
mkrafiul13453 <mkrafiul13453@gamil.com>
2026-01-29 19:38:43 +0600

    task-2 done

---
+++ task-2.js
@@ -0,0 +1,8 @@
+const destination=["dhaka","cumilla","gazipur","foridpur","shoriotpur"];
+destination.push("cox's bazar","sylhet","khulna");
+console.log(destination);
+
+console.log(destination.includes("sylhet","khulna"));
+
+destination.pop();
+console.log(destination);
